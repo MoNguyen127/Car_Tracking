0611aa037a367c7f2ccef3d5b7afc6c05032060e
User <User@DESKTOP-UTIESKD>
2021-06-23 14:10:49 +0700

    5

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-     public partial class Form1 : Form
+     public partial class Form1 : DevComponents.DotNetBar.Office2007RibbonForm
      {
           public Form1()
           {
